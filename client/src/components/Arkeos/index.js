0d2e3059079a4ad78e769d8232bb2c6eda519902
floriangrv <florian.gravepro@gmail.com>
2021-06-10 17:46:12 +0200

    side menu

--- client/src/components/Arkeos/index.js
+++ client/src/components/Arkeos/index.js
@@ -1,14 +1,23 @@
 // == Import npm
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 // == Import
+import LeftMenu from '../LeftMenu';
 import './styles.scss';
 
 // == Composant
 const Arkeos = () => (
+  <Router>
   <div className="arkeos">
-    <h1>Composant : Arkeos</h1>
+    <LeftMenu />
   </div>
+  </Router>
 );
 
 // == Export
---
+++ client/src/components/Message/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Message() {
+  return (
+    <div>
+      <h1> je suis le message</h1>
+    </div>
+  )
+}
+
+export default Message
