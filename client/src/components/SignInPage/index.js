2baf3a205376d7c7d0716b3167754858e888d09d
floriangrv <florian.gravepro@gmail.com>
2021-06-16 12:15:34 +0200

    fix css button

--- client/src/components/SignInPage/index.js
+++ client/src/components/SignInPage/index.js
@@ -5,9 +5,9 @@ import "./styles.scss";
 const SignInPage = () => {
   return (
     <form id="signup-form">
-      <label htmlFor="email">Email</label>
+      <label htmlFor="email"></label>
       <input placeholder="Email" type="text" name="email" id="email" required />
-      <label htmlFor="password">Mot de passe</label>
+      <label htmlFor="password"></label>
       <input
         type="password"
         placeholder="Mot de passe"
@@ -16,7 +16,7 @@ const SignInPage = () => {
         id="password"
         required
       />
-      <input type="submit" value="Se connecter" />
+      <input id="submit-button" type="submit" value="Se connecter" />
     </form>
   );
 };
