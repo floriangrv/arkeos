924760edcdb3a903d5cf6923fb2de9ff214d665d
floriangrv <florian.gravepro@gmail.com>
2021-06-29 16:50:42 +0200

    redirect markeplace

--- client/src/components/Redirect_page/index.js
+++ client/src/components/Redirect_page/index.js
@@ -1,25 +1,20 @@
-import React from 'react'
-import cameleon_vector from '../../assets/images/cameleon_vector.svg'
-import './style.css'
+import React from "react";
+import cameleon_vector from "../../assets/images/cameleon_vector.svg";
+import "./style.css";
 
 const Redirect_page = () => {
   return (
+    <div className="Redirect_container">
+      <div className="Redirect">
+        <h1 className="Oops"> Oops !</h1>
+        <div className="Redirect_picture_container">
+          <img className="Redirect_picture" src={cameleon_vector} />
+        </div>
 
-    <div className= 'Redirect_container'>
-       <div className= 'Redirect'>
-       <h1 className= 'Oops'> Oops !</h1>
-<div className= 'Redirect_picture_container'>
-  <img className= 'Redirect_picture' src={cameleon_vector} />
-</div>
-      
-
-     
-      <p> Vous devez vous connecter pour accéder à cette page :)</p>
+        <p> Vous devez vous connecter pour accéder à cette page :</p>
+      </div>
     </div>
+  );
+};
 
-    </div>
-   
-  )
-}
-
-export default Redirect_page
+export default Redirect_page;
