bd95d9063ecef4a830a0ff4272516530012da66c
Myrmelo <nicolas_mu@protonmail.com>
2021-06-29 11:04:14 +0200

    orange

--- client/src/components/Agenda/index.js
+++ client/src/components/Agenda/index.js
@@ -6,7 +6,9 @@ const Agenda = () => (
   <>
   <Add_event />
 <div className= "Agenda_container">
-<iframe className= "Agenda" src="https://calendar.google.com/calendar/embed?src=dv65fm3h31jdg5g41de7nti6qk%40group.calendar.google.com&ctz=Europe%2FParis"></iframe>
+
+
+  
 </div>
 </>
 )
